8606827864dd9a29a9d5d20340f642d7a1c1ce45
Haris Javed <harisj58@gmail.com>
2025-01-28 17:18:04 +0000

    chore: setup proper layout for application

--- app/layout.js
+++ app/layout.js
@@ -1,16 +1,8 @@
-import localFont from "next/font/local";
 import "./globals.css";
+import { Inter } from "next/font/google"
+import { ThemeProvider } from "next-themes";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: "Create Next App",
@@ -20,8 +12,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+      <body className={`${inter.className} dark`}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem={false}
+          storageKey="portfolio-theme">
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
